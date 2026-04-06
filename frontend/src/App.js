872e9da1508f14625151c48d96c5bd9eabdd29da
Sai Greeshmitha <greeshmithagowd@gmail.com>
2026-04-06 12:28:51 +0530

    Added frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-primary">Job Posting Platform</h1>
+      <button className="btn btn-success">Get Started</button>
+    </div>
+  );
+}
+
+export default App;
